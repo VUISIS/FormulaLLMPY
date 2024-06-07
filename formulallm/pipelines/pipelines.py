14af9e0ff5ec978fe77fe1754f7dcdbd75904e4d
liy94 <yifei.li@vanderbilt.edu>
2024-06-07 13:38:26 -0500

    modified userproxy_agent's name, updated pipeline

---
+++ formulallm/pipelines/pipelines.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from agents import executor_agent, fixer_agent, interpreter_agent, userproxy_agent
