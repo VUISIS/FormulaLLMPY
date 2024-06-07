a648776f1726373828cfe5a0db8a80702f383e84
liy94 <yifei.li@vanderbilt.edu>
2024-06-07 13:29:30 -0500

    correct name of userproxy_agent in  __init__

--- formulallm/agents/__init__.py
+++ formulallm/agents/__init__.py
@@ -1,6 +1,6 @@
 from .executor_agent import executor_agent
 from .fixer_agent import fixer_agent
 from .interpreter_agent import interpreter_agent
-from .userproxy_agent import user_proxy
+from .userproxy_agent import userproxy_agent
 
-__all__ = ['executor_agent', 'fixer_agent', 'interpreter_agent', 'user_proxy']
+__all__ = ['executor_agent', 'fixer_agent', 'interpreter_agent', 'userproxy_agent']
