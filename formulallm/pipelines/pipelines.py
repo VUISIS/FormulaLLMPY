df4155ce97a679e72075014e07b67bcc8e5bbd03
VeraZhang0311 <tianzi.zhang@vanderbilt.edu>
2024-06-07 14:34:21 -0500

    groupchat first try

--- formulallm/pipelines/pipelines.py
+++ formulallm/pipelines/pipelines.py
@@ -5,3 +5,37 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from agents import executor_agent, fixer_agent, interpreter_agent, userproxy_agent
+from autogen import GroupChat, GroupChatManager
+from formula.formula_program import *
+
+local_llm_config={
+    "config_list": [
+        {
+            "model": "NotRequired", # Loaded with LiteLLM command
+            "api_key": "NotRequired", # Not needed
+            "base_url": "http://0.0.0.0:4000"  # Your LiteLLM URL
+        }
+    ],
+    "cache_seed": None # Turns off caching, useful for testing different models
+}
+
+code = load("/home/zhang0311/FormulaLLMPY/examples/data/MappingExample.4ml")
+
+task = f"First read the following code written in a DSL called formula to understand the syntax. \nThen perform the tasks according to the user's instruction! \n\n{code}"
+
+
+
+groupchat = GroupChat(
+    agents=[userproxy_agent, fixer_agent, executor_agent, interpreter_agent],
+    messages=[],
+    max_round=10
+)
+
+manager = GroupChatManager(
+    groupchat=groupchat, llm_config=local_llm_config
+)
+
+groupchat_result = userproxy_agent.initiate_chat(
+    manager,
+    message=task,
+)
