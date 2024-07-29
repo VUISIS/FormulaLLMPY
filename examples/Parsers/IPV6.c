f26e36ee90cffb769adc3ae3b045a3115a547c52
liy94 <yifei.li@vanderbilt.edu>
2024-07-29 11:21:51 -0500

    formula code for IPV6 parser

--- examples/Parsers/IPV6.c
+++ examples/Parsers/IPV6.c
@@ -6,7 +6,8 @@
 #include <errno.h>
 
 // Function to convert a single hexadecimal digit to its integer value
-static int hex_digit_to_int(char c) {
+static int hex_digit_to_int(char c)
+{
     if (isdigit(c))
         return c - '0';
     else if (isxdigit(c))
@@ -16,8 +17,10 @@ static int hex_digit_to_int(char c) {
 }
 
 // Simplified inet_pton implementation for IPv6
-int inet_pton(int af, const char *src, void *dst) {
-    if (af != AF_INET6) {
+int inet_pton(int af, const char *src, void *dst)
+{
+    if (af != AF_INET6)
+    {
         errno = EAFNOSUPPORT;
         return -1;
     }
@@ -29,34 +32,50 @@ int inet_pton(int af, const char *src, void *dst) {
     const char *endp = src + strlen(src);
     int saw_digit = 0, colonp = -1;
 
-    while (curtok < endp) {
+    while (curtok < endp)
+    {
         int ch = *curtok++;
         int val = hex_digit_to_int(ch);
 
-        if (val != -1) {
-            if (saw_digit == 0) {
+        if (val != -1)
+        {
+            if (saw_digit == 0)
+            {
                 *tp = val;
                 saw_digit = 1;
-            } else {
-                if (*tp & 0xf0) return 0; // Overflow
+            }
+            else
+            {
+                if (*tp & 0xf0)
+                    return 0; // Overflow
                 *tp = (*tp << 4) | val;
             }
-        } else if (ch == ':') {
+        }
+        else if (ch == ':')
+        {
             curtok++; // Skip the colon
-            if (!saw_digit) {
-                if (colonp != -1) return 0; // Multiple "::" found
+            if (!saw_digit)
+            {
+                if (colonp != -1)
+                    return 0; // Multiple "::" found
                 colonp = tp - tmp;
-            } else if (curtok == endp) {
+            }
+            else if (curtok == endp)
+            {
                 return 0; // Trailing ":" is invalid
             }
             saw_digit = 0;
-        } else {
+        }
+        else
+        {
             return 0; // Invalid character
         }
-        if (saw_digit) tp++;
+        if (saw_digit)
+            tp++;
     }
 
-    if (colonp != -1) {
+    if (colonp != -1)
+    {
         memmove(tmp + sizeof(tmp) - (tp - tmp), tmp + colonp, tp - tmp - colonp);
         memset(tmp + colonp, 0, sizeof(tmp) - (tp - tmp));
     }
